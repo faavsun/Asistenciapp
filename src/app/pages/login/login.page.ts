e68784e08c94338a7660fdcfb47c119200ddc118
faavsun <f.aburto@duocuc.cl>
2024-11-26 23:12:59 -0300

    login local

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,10 @@
 
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { UtilsService } from 'src/app/services/utils.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
@@ -7,22 +12,86 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
-  constructor(private localStorageService: LocalStorageService) {}
+export class LoginPage implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
 
-  login(user: any): void {
-    const userData = { id: user.id, name: user.name, token: 'fake-token' };
-    this.localStorageService.setItem('user', userData);
-    console.log('Usuario guardado en localStorage:', userData);
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+  localStorageSvc = inject(LocalStorageService); // Servicio de localStorage
+
+  constructor(private menuCtrl: MenuController) { }
+
+  ngOnInit() {
+    this.menuCtrl.enable(false); // Desactivar el menú en esta vista
   }
 
-  getUserData(): void {
-    const user = this.localStorageService.getItem('user');
-    console.log('Datos del usuario desde localStorage:', user);
+  async submit() {
+    if (this.form.valid) {
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      // Reinicia los datos del usuario en el servicio
+      this.firebaseSvc.resetUserData();
+
+      this.firebaseSvc.signIn(this.form.value as User).then(res => {
+        this.getUserInfo(res.user.uid);
+      }).catch(error => {
+        console.log(error);
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2000,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        });
+      }).finally(() => {
+        loading.dismiss();
+      });
+    }
   }
 
-  logout(): void {
-    this.localStorageService.removeItem('user');
-    console.log('Usuario eliminado de localStorage');
+  async getUserInfo(uid: string) {
+    if (this.form.valid) {
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      let path = `users/${uid}`;
+
+      this.firebaseSvc.getDocument(path).then((user: User) => {
+        if (user) {
+          this.utilsSvc.saveInLocalStorage('user', user); // Guardar en localStorage usando Utils
+          this.localStorageSvc.setItem('user_backup', user); // Guardar como respaldo en localStorage directamente
+
+          // Verificar el tipo de usuario y redirigir a la ruta correspondiente
+          if (user.tipo === 'estudiante') {
+            this.utilsSvc.routerLink('/main-estudiante/home');
+          } else if (user.tipo === 'profesor') {
+            this.utilsSvc.routerLink('/main-profesor/home-profesor');
+          }
+
+          this.form.reset();
+          this.utilsSvc.presentToast({
+            message: `Te damos la bienvenida ${user.name}`,
+            duration: 1500,
+            color: 'primary',
+            position: 'middle',
+            icon: 'person-circle-outline'
+          });
+        } else {
+          console.error('No se encontraron datos del usuario.');
+        }
+      }).catch(error => {
+        console.log(error);
+      }).finally(() => {
+        loading.dismiss();
+      });
+    }
+  }
+
+  recuperar() {
+    this.utilsSvc.routerLink('/olvidada');
   }
 }
